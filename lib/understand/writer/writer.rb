1b16c552c94b90975753017d2d2ba0796e4c2281
Salvatore <salvatorepiazzolla3@gmail.com>
2014-02-06 14:37:26 +0100

    add rails log info ot write system

--- lib/understand/writer/writer.rb
+++ lib/understand/writer/writer.rb
@@ -11,16 +11,19 @@ module Writer
     	response 	= self.write_log(log_string)
 	
     	unless response
-    		if File.directory?(Rails.root + Base::LOG_FILE_PATH) && File.directory?(Rails.root + Base::LOG_FILE_PATH + "processes_log/")
-    			raise '\n\n--The understand log directory already excist--\n\n'
-    		end 
     		if File.directory?(Rails.root + Base::LOG_FILE_PATH) == false
     			`mkdir #{Rails.root + Base::LOG_FILE_PATH}`
+                Rails.logger.info "\n\n-- mkdir #{Rails.root + Base::LOG_FILE_PATH} --\n\n"
     		end
 
     		if File.directory?(Rails.root + Base::LOG_FILE_PATH + "processes_log/") == false
     			`mkdir #{Rails.root + Base::LOG_FILE_PATH}processes_log/`
+                Rails.logger.info "\n\n-- mkdir #{Rails.root + Base::LOG_FILE_PATH}processes_log/ --\n\n"
     		end
+
+            if File.directory?(Rails.root + Base::LOG_FILE_PATH) && File.directory?(Rails.root + Base::LOG_FILE_PATH + "processes_log/")
+                raise "\n\n-- The understand log directory already excist --\n\n"
+            end 
 
     		self.write_log(log_string)
     	end
